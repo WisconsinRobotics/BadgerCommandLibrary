72c357b18dd9e927482a5af233786c276837ac8b
ShaohengZhou <szhou228@wisc.edu>
2017-09-28 19:58:10 -0500

    New MeControl.c.h, with TankDrive and AllWheelSpd

--- robot/urc2018/include/MechanicalControlPackets.h
+++ robot/urc2018/include/MechanicalControlPackets.h
@@ -11,13 +11,57 @@ extern "C" {
 
 /* Payload definitions */
 
+	typedef struct TankDrivePayload
+	{
+		int8_t left;
+		int8_t right;
+	} TankDrivePayload;
+
+	typedef struct AllWheelSpeedPayload
+	{
+		int8_t front_left;
+		int8_t mid_left;
+		int8_t back_left;
+		int8_t front_right;
+		int8_t mid_right;
+		int8_t back_right;
+	} AllWheelSpeedPayload;
+
 /* Initialization function definitions */
+	BCL_STATUS InitializeSetTankDriveSpeedPacket(
+		BclPacket *pkt, TankDrivePayload *payload);
+	BCL_STATUS InitializeAllWheelSpeedPacket(
+		BclPacket *pkt, AllWheelSpeedPayload *payload);
 
 /* Serialization function definitions */
+	BCL_STATUS SerializeAllWheelSpeedPayload(
+		const BclPayloadPtr     payload,
+		uint8_t *               buffer,
+		uint8_t                 bufferLength,
+		uint8_t *               bytes_written
+	);
+
+	BCL_STATUS SerializeTankDriveSpeedPayload(
+		const BclPayloadPtr     payload,
+		uint8_t *               buffer,
+		uint8_t                 bufferLength,
+		uint8_t *               bytes_written
+	);
 
 /* Deserialization function definitions */
+	BCL_STATUS DeserializeAllWheelSpeedPayload(
+		const BclPayloadPtr     payload,
+		uint8_t *				buffer,
+		uint8_t                 bufferlength,
+		uint8_t *               bytes_read
+	);
 
-
+	BCL_STATUS DeserializeTankDriveSpeedPayload(
+		const BclPayloadPtr           payload,
+		uint8_t *				  buffer,
+		uint8_t                 bufferlength,
+		uint8_t *               bytes_read
+	);
 #ifdef __cplusplus
 }
 #endif
--- robot/urc2018/src/MechanicalControlPackets.c
+++ robot/urc2018/src/MechanicalControlPackets.c
@@ -1,3 +1,133 @@
 #include "MechanicalControlPackets.h"
 #include "Packet.h"
 #include <string.h>
+
+BCL_STATUS InitializeSetTankDriveSpeedPacket(
+	BclPacket *pkt, TankDrivePayload *payload) {
+
+	return InitializeBclPacket(
+		pkt,
+		SET_TANK_DRIVE_SPEED,
+		payload,
+		sizeof(TankDrivePayload),
+		&SerializeTankDriveSpeedPayload,
+		&DeserializeTankDriveSpeedPayload
+	);
+}
+
+BCL_STATUS InitializeAllWheelSpeedPacket(
+	BclPacket *pkt, AllWheelSpeedPayload *payload) {
+	return InitializeBclPacket(
+		pkt,
+		SET_ALL_WHEEL_SPEED,
+		payload,
+		sizeof(AllWheelSpeedPayload),
+		&SerializeAllWheelSpeedPayload,
+		&DeserializeAllWheelSpeedPayload
+	);
+}
+
+BCL_STATUS SerializeAllWheelSpeedPayload(
+	const BclPayloadPtr     payload,
+	uint8_t *               buffer,
+	uint8_t                 bufferLength,
+	uint8_t *               bytes_written
+) {
+	const AllWheelSpeedPayload *awsp;
+	if (!payload || !buffer)
+		return BCL_INVALID_PARAMETER;
+	if (bufferLength < sizeof(uint8_t) + 6*sizeof(int8_t))
+		return BCL_BUFFER_TOO_SMALL;
+
+	awsp = (const AllWheelSpeedPayload*)payload;
+	buffer[FRONT_LEFT_WHEEL] = awsp->front_left;
+	buffer[MIDDLE_LEFT_WHEEL] = awsp->mid_left;
+	buffer[BACK_LEFT_WHEEL] = awsp->back_left;
+	buffer[FRONT_RIGHT_WHEEL] = awsp->front_right;
+	buffer[MIDDLE_RIGHT_WHEEL] = awsp->mid_right;
+	buffer[BACK_RIGHT_WHEEL] = awsp->back_right;
+
+	if (bytes_written)
+		*bytes_written = sizeof(uint8_t) + sizeof(*awsp);
+	return BCL_OK;
+}
+
+BCL_STATUS SerializeTankDriveSpeedPayload(
+	const BclPayloadPtr     payload,
+	uint8_t *               buffer,
+	uint8_t                 bufferLength,
+	uint8_t *               bytes_written
+)
+{
+	const TankDrivePayload *tdp;
+	if (!payload || !buffer)
+		return BCL_INVALID_PARAMETER;
+	if (bufferLength < 2*sizeof(int8_t))
+		return BCL_BUFFER_TOO_SMALL;
+
+	tdp = (const TankDrivePayload*)payload;
+	buffer[0] = tdp->left;
+	buffer[1] = tdp->right;
+
+	if (bytes_written)
+		*bytes_written = sizeof(*tdp);
+	return BCL_OK;
+}
+
+
+BCL_STATUS DeserializeAllWheelSpeedPayload(
+	const BclPayloadPtr     payload,
+	uint8_t *				buffer,
+	uint8_t                 bufferLength,
+	uint8_t *               bytes_read
+)
+{
+	if (!payload || !buffer)
+		return BCL_INVALID_PARAMETER;
+	if (bufferLength < sizeof(uint8_t) + 6*sizeof(int8_t))//why???????? WILLLLLLL!
+		return BCL_BUFFER_TOO_SMALL;
+
+	AllWheelSpeedPayload *awsp;
+
+	awsp = (AllWheelSpeedPayload*)(payload);
+	awsp->front_left = buffer[FRONT_LEFT_WHEEL];
+	awsp->mid_left = buffer[MIDDLE_LEFT_WHEEL];
+	awsp->back_left = buffer[BACK_LEFT_WHEEL];
+	awsp->front_right = buffer[FRONT_RIGHT_WHEEL];
+	awsp->mid_right = buffer[MIDDLE_RIGHT_WHEEL];
+	awsp->back_right = buffer[BACK_RIGHT_WHEEL];
+
+
+	if (bytes_read)
+		*bytes_read = sizeof(uint8_t) + sizeof(*awsp);
+
+	return BCL_OK;
+}
+
+
+
+
+
+BCL_STATUS DeserializeTankDriveSpeedPayload(
+	const BclPayloadPtr           payload,
+	uint8_t *				  buffer,
+	uint8_t                 bufferLength,
+	uint8_t *               bytes_read
+	)
+{
+	if (!payload || !buffer)
+		return BCL_INVALID_PARAMETER;
+	if (bufferLength < 2*sizeof(int8_t))
+		return BCL_BUFFER_TOO_SMALL;
+
+	TankDrivePayload *tdp;
+
+	tdp = (TankDrivePayload*)(payload);
+	tdp->left = buffer[0];
+	tdp->right = buffer[1];
+
+	if (bytes_read)
+		*bytes_read = 2*sizeof(int8_t);
+
+	return BCL_OK;
+}
