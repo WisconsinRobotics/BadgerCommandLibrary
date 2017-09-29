d1cbf6a9e407fb286aa473d4b68be9295700dbf9
Winor Chen <wchen329@wisc.edu>
2017-09-28 20:09:20 -0500

    Added all wheel, wheel speed testing.

--- robot/atlas/include/AtlasCommon.h
+++ robot/atlas/include/AtlasCommon.h
@@ -6,7 +6,8 @@
 /* Debug packets (0x00 - 0x1F) */
 
 /* Mechanical packets (0x20 - 0x4F) */
-
+#define SET_TANK_DRIVE_SPEED		0x20
+#define SET_ALL_WHEEL_SPEED		0x21
 
 /* Other Atlas definitions */
 #define FRONT_LEFT_WHEEL                0
--- robot/atlas/include/MechanicalControlPackets.h
+++ robot/atlas/include/MechanicalControlPackets.h
@@ -11,12 +11,60 @@ extern "C" {
 
 /* Payload definitions */
 
+	typedef struct TankDrivePayload
+	{
+		int8_t left;
+		int8_t right;
+	} TankDrivePayload;
+
+	typedef struct AllWheelSpeedPayload
+	{
+		int8_t front_left;
+		int8_t back_left;
+		int8_t front_right;
+		int8_t back_right;
+	} AllWheelSpeedPayload;
+
 /* Initialization function definitions */
+	BCL_STATUS InitializeSetTankDriveSpeedPacket(
+		BclPacket *pkt,
+		TankDrivePayload *payload
+	);
+
+	BCL_STATUS InitializeSetAllWheelSpeedPacket(
+		BclPacket *pkt,
+		AllWheelSpeedPayload *payload
+	);
 
 /* Serialization function definitions */
+	BCL_STATUS SerializeTankDriveSpeedPayload(
+		const BclPayloadPtr     payload,
+		uint8_t *               buffer,
+		uint8_t                 length,
+		uint8_t *               bytes_written
+	);
+
+	BCL_STATUS SerializeAllWheelSpeedPayload(
+		const BclPayloadPtr     payload,
+		uint8_t *               buffer,
+		uint8_t                 length,
+		uint8_t *               bytes_written
+	);
 
 /* Deserialization function definitions */
+	BCL_STATUS DeserializeTankDriveSpeedPayload(
+		BclPayloadPtr           payload,
+		const uint8_t *         buffer,
+		uint8_t                 length,
+		uint8_t *               bytes_read
+	);
 
+	BCL_STATUS DeserializeAllWheelSpeedPayload(
+		BclPayloadPtr           payload,
+		const uint8_t *         buffer,
+		uint8_t                 length,
+		uint8_t *               bytes_read
+	);
 
 #ifdef __cplusplus
 }
